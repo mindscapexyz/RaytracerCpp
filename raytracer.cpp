4cb89216ebce4cd12e44fb5629240ee4c438fadf
mindscapexyz <mhalimshukor@gmail.com>
2024-05-01 10:14:48 +0800

    basic classes

--- raytracer.cpp
+++ raytracer.cpp
@@ -1,13 +1,40 @@
 #include <fstream>
 
+class Vec {
+    double x,y,z;
+    Vec(){x=y=z=0;}
+    Vec(double a, double b, double c){x=a,y=b,z=c;}
+};
+
+class Ray{
+    Vec o; // Origin
+    Vec d; // Direction
+    Ray(Vec i, Vec j){o=i,d=j;}
+};
+
+class Sphere {
+    Vec c;
+    double r;
+    Sphere(Vec i, double j){c=i, r=j;};
+    bool intersect(Ray ray, double &t){
+        Vec o = ray.o;
+        Vec d = ray.d;
+        Vec oc = o-c;
+        double b = 2*dot(oc,d);
+    }
+}
 
 int main(){
     const int W{500};
     const int H{500};
 
     for(int y= 0; y<H; y++){
-        for(int x=0; x<H; x++){
+        for(int x=0; x<W; x++){
             // Send ray through each pixel
+            Ray ray(Vec(x,y,0), Vec(0,0,1));
+
+            // Check for intersections
+
         }
     }
 }
