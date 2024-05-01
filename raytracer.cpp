1927c899e7836f4a0cc2ed746f9ac42333d265f6
mindscapexyz <mhalimshukor@gmail.com>
2024-05-01 10:06:21 +0800

    simple raytracer

---
+++ raytracer.cpp
@@ -0,0 +1,13 @@
+#include <fstream>
+
+
+int main(){
+    const int W{500};
+    const int H{500};
+
+    for(int y= 0; y<H; y++){
+        for(int x=0; x<H; x++){
+            // Send ray through each pixel
+        }
+    }
+}
